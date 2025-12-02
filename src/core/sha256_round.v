597d62b08404188db44134bc64f998508edeedb8
Sovereign Shadow <153613961+securitybrahh@users.noreply.github.com>
2025-12-02 06:44:43 -0600

    Rename module to sha256_round_stage and update inputs

--- src/core/sha256_round.v
+++ src/core/sha256_round.v
@@ -1,13 +1,9 @@
-module sha256_round(
-    input  wire [31:0] a, b, c, d, e, f, g, h,
-    input  wire [31:0] w,
-    input  wire [31:0] k,
-    output wire [31:0] a_out, b_out, c_out, d_out,
-    output wire [31:0] e_out, f_out, g_out, h_out
+module sha256_round_stage (
+    input [31:0] a, b, c, d, e, f, g, h,  // Current state variables
+    input [31:0] w,                       // Current message schedule value
+    input [31:0] k,                       // SHA256 constant for this round
+    output [31:0] a_out, b_out, c_out, d_out, e_out, f_out, g_out, h_out
 );
-
-    // Include functions
-    `include "sha256_functions.vh"
 
     wire [31:0] T1 = h + Sigma1(e) + Ch(e,f,g) + k + w;
     wire [31:0] T2 = Sigma0(a) + Maj(a,b,c);
